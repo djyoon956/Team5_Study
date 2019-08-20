ee4f9466caae3ac18786235e7a4cd1dc7e1aaa58
hyungnam <zzzsh789@naver.com>
2019-08-20 23:35:54 +0900

    "성공"

--- src/ch05/hyungnam/Ex05_practice.java
+++ src/ch05/hyungnam/Ex05_practice.java
@@ -5,6 +5,9 @@ public class Ex05_practice {
 	public static void main(String[] args) {
 		System.out.println("d");
 		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
+		System.out.println("hello world");
 	}
 
 }
