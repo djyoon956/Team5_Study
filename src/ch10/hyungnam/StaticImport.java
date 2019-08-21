cc9ba55016eff1dba448c575c6dbaa38448509c6
hyungnam <zzzsh789@naver.com>
2019-08-21 17:43:00 +0900

    챕터 9, 10 업로드

---
+++ src/ch10/hyungnam/StaticImport.java
@@ -0,0 +1,13 @@
+package ch10.hyungnam;
+import static java.lang.Math.*;
+public class StaticImport {
+
+	public static void main(String[] args) {
+		System.out.println(E);
+		System.out.println(PI);
+		System.out.println(abs(-55)); //절대값 반환
+		System.out.println(max(77,88));
+		System.out.println(min(33,55));
+	}
+
+}
