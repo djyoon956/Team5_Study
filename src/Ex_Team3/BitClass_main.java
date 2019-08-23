9cd5b1aeb7cd953631c0e74c94f3d4f414d89555
윤다정 <djyoon956@gmail.com>
2019-08-23 16:01:00 +0900

    add 3조 main

---
+++ src/Ex_Team3/BitClass_main.java
@@ -0,0 +1,12 @@
+package Ex_Team3;
+
+public class BitClass_main {
+	public static void main(String[] args) {
+//		BitClass bit1 = new BitClass("흥부", "010-");
+//		bit1.printInfo();
+//		bit1.apply();
+		BitClass bit2 = new BitClass("놀부", "011-", "1234-56789");
+		bit2.printInfo();
+		bit2.apply();
+	}
+}
