ff66ecda3f68ea7444c5d07444fd50192c10c963
hyungnam <zzzsh789@naver.com>
2019-08-26 19:34:42 +0900

    no message

--- src/quiz_week01/quiz_week01_hn.java
+++ src/quiz_week01/quiz_week01_hn.java
@@ -1,9 +1,20 @@
 package quiz_week01;
 
+import java.util.Scanner;
+
 public class quiz_week01_hn {
 
 	public static void main(String[] args) {
 		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("출력할 구구단의 단 수: ");
+		int num=sc.nextInt();
+		for(int i=1; i<10; i++) {
+			for(int j=num-1; j<=num+1; j++) {
+				System.out.print(j + " * " + i +" = " + j*i + "\t\t");
+			}
+			System.out.println("");
+		}
 	}
 
 }
