1415fc252d5f1e80751561f5e5ab2f7cb1bb6ea2
hyungnam <zzzsh789@naver.com>
2019-08-28 12:19:26 +0900

    1주차 2번째 스터디문제 업데이트

--- src/quiz_week01_02/hyungnam/Quiz_week01_02_hn.java
+++ src/quiz_week01_02/hyungnam/Quiz_week01_02_hn.java
@@ -1,10 +1,36 @@
 package quiz_week01_02.hyungnam;
+import java.util.Scanner;
 
 public class Quiz_week01_02_hn {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner=new Scanner(System.in);
+		String str=scanner.nextLine();
+		
+		switch (str) {
+		case "짝수": 
+			for(int i=2; i<10; i+=2) {
+				System.out.println(i+ " 단 =======");
+				for(int j=1; j<10; j++) {
+					System.out.println(i + " * " + j + " = " + i*j);
+				}
+			}
+			break;
+			
+		case "홀수":
+			for (int i = 2; i < 10; i++) {
+				if (i % 2 == 1) {
+					System.out.println(i + " 단 =======");
+					for (int j = 1; j < 10; j++) {
+						System.out.println(i + " * " + j + " = " + i * j);
+					}
+				}
+			}
+			break;
 
+		default: System.out.println("짝수나 홀수를 입력해주세요");
+			break;
+		}
 	}
 
 }
