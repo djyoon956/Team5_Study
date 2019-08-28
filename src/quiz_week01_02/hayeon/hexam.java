161e42555e91806268fe729a2553eedaa92c9087
이하연 <hayeony0124@naver.com>
2019-08-28 12:30:28 +0900

    ㅇㅇ

---
+++ src/quiz_week01_02/hayeon/hexam.java
@@ -0,0 +1,39 @@
+package quiz_week01_02.hayeon;
+
+/*	
+import java.util.Scanner;
+
+public class hexam {
+
+	public static void main(String[] args) {
+		
+		String name = "홀수";
+		String name2="짝수";
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("홀수인지 짝수인지 입력하시오.");
+		name = sc.toString();
+		name2 = sc.toString();
+	
+	
+		if(name) { //홀수
+			for(i=1; i%2==1; i++) {
+				for(j=1; j<=9; j++) {
+					System.out.println(i*j);
+				}
+			}
+		}
+		else if(name2) { //짝수
+			for(q=2; q%2==0; q++) {
+				for(w=1; w<=9; w++) {
+					System.out.println(q*w);
+				}
+			}
+		}
+		else {
+			System.out.println("홀수 또는 짝수가 아닙니다.");
+		}
+	}
+
+}
+*/
