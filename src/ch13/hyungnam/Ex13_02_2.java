250c33c508a4f7b473a306c0ac6e69b532fb22f6
hyungnam <zzzsh789@naver.com>
2019-08-29 14:30:23 +0900

    no message

--- src/ch13/hyungnam/Ex13_02_2.java
+++ src/ch13/hyungnam/Ex13_02_2.java
@@ -1,5 +1,7 @@
 package ch13.hyungnam;
 
+import java.util.Arrays;
+
 public class Ex13_02_2 {
 
 	public static void main(String[] args) {
@@ -9,43 +11,62 @@ public class Ex13_02_2 {
 				{7,8,9}
 		};
 		Ex13_02_2Test ex13_02_2Test=new Ex13_02_2Test();
-		ex13_02_2Test.shift(arr);
 		ex13_02_2Test.showArr(arr);
 		ex13_02_2Test.shift(arr);
-		ex13_02_2Test.showArr(arr);
 		ex13_02_2Test.shift(arr);
+		System.out.println(">>>>>>>>>>Shift");
 		ex13_02_2Test.showArr(arr);
+//		ex13_02_2Test.shift(arr);
+//		ex13_02_2Test.showArr(arr);
+//		ex13_02_2Test.shift(arr);
+//		ex13_02_2Test.showArr(arr);
 	}
 }
 
 class Ex13_02_2Test{
-	int[] temp=new int[3];
+	public void shift(int[][] arr) {
+		int[] temp=arr[2];
+		
+		for(int i=arr.length-1; i>=1; i--) {
+			arr[i]=arr[i-1];
+		}
+		arr[0]=temp;
+	}
 	
 	public void showArr(int[][] arr) {
-		for(int i=0; i<arr.length; i++) {
+		for(int i=0; i<arr.length; i++	) {
 			for(int j=0; j<arr[i].length; j++) {
 				System.out.print(arr[i][j]);
 			}
 			System.out.println("");
 		}
 	}
-	public void shift(int[][] arr) {
-		for(int i=0; i<3; i++) {
-			for(int j=0; j<3; j++) {
-				temp[i]=arr[2][i];
-			}
-		}
-		
-		for(int i=arr.length-1; i>=1; i--) {
-			for(int j=0; j<arr[i].length; j++) {
-				arr[i][j]=arr[i-1][j];
-			}
-		}
-		
-			for(int i=0; i<3; i++) {
-					arr[0][i]=temp[i];
-			}
+//	int[] temp=new int[3];
+//	
+//	public void showArr(int[][] arr) {
+//		for(int i=0; i<arr.length; i++) {
+//			for(int j=0; j<arr[i].length; j++) {
+//				System.out.print(arr[i][j]);
+//			}
+//			System.out.println("");
+//		}
+//	}
+//	public void shift(int[][] arr) {
+//		for(int i=0; i<3; i++) {
+//			for(int j=0; j<3; j++) {
+//				temp[i]=arr[2][i];
+//			}
+//		}
+//		
+//		for(int i=arr.length-1; i>=1; i--) {
+//			for(int j=0; j<arr[i].length; j++) {
+//				arr[i][j]=arr[i-1][j];
+//			}
+//		}
+//		
+//			for(int i=0; i<3; i++) {
+//					arr[0][i]=temp[i];
+//			}
 			
-	}
 	
 }
