15fe22c5dc35253b41ac0df60a2a8deabbeec251
hyungnam <zzzsh789@naver.com>
2019-08-30 22:30:06 +0900

    1주차 3번째 문제(주말문제)

--- src/quiz_week01_03/hyungnam/Quiz_week01_03_hn.java
+++ src/quiz_week01_03/hyungnam/Quiz_week01_03_hn.java
@@ -3,7 +3,9 @@ package quiz_week01_03.hyungnam;
 public class Quiz_week01_03_hn {
 
 	public static void main(String[] args) {
-		// 여기에 문제설명(시나리오)해주세요~
+		/*
+		 * 상속관계 기반의 다형성에 간단한 예를 작성해주세요.
+		 */
 	}
 
 }
