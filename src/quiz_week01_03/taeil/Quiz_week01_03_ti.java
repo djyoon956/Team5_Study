22ad21a4783c61f7f31cdb4664d70d354d2b4c37
Kwon <taeik0303@naver.com>
2019-09-02 19:17:03 +0900

    add

--- src/quiz_week01_03/taeil/Quiz_week01_03_ti.java
+++ src/quiz_week01_03/taeil/Quiz_week01_03_ti.java
@@ -1,5 +1,31 @@
 package quiz_week01_03.taeil;
 
+
+
+class Shape2{
+	 int r = 3;
+	 
+	}
+
+
+class Circle2 extends Shape2 {
+	Circle2(){
+	System.out.println("���� ���̴� : " + r * r * 3.14);
+	}
+}
+
 public class Quiz_week01_03_ti {
-	// 상속을 사용해서 반지름이 3인 원의 넓이를 구하세요
+
+	public static void main(String[] args) {
+		
+		Shape2 shape = new Shape2();
+		Circle2 circle = new Circle2();
+		
+		
+		
+	
+	
+	
+	}
+
 }
