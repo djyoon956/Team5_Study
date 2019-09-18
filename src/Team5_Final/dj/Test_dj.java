5a8d01884d24bd7ce876ece39cb82ec177baabb5
dajeong.yoon <djyoon956@gmail.com>
2019-09-18 20:21:38 +0900

    admin 로그인 로그아웃 기능 추가

--- src/Team5_Final/dj/Test_dj.java
+++ src/Team5_Final/dj/Test_dj.java
@@ -1,21 +1,8 @@
 package Team5_Final.dj;
 
-import java.util.Scanner;
-
 public class Test_dj {
-
-	private final String ADMIN_ID = "admin";
-	private final String ADMIN_PW = "123";
-
-	private void adminLogin() {
-		Scanner scanner = new Scanner(System.in);
-		System.out.print("ID : ");
-		String id = scanner.next();
-		System.out.print("Password : ");
-		String password = scanner.next();
-	}
-
-	private void adminLogout() {
-
+	public static void main(String[] args) {
+		PCmanagement p = new PCmanagement();
+		p.start();
 	}
 }
