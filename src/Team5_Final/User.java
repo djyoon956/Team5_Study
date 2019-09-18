831b0ed60247486e09e9a196516c2f317e8a41d1
dajeong.yoon <djyoon956@gmail.com>
2019-09-18 20:33:38 +0900

    User Serializable 인터페이스 상속

--- src/Team5_Final/User.java
+++ src/Team5_Final/User.java
@@ -1,6 +1,8 @@
 package Team5_Final;
 
-public class User {
+import java.io.Serializable;
+
+public class User implements Serializable {
 	private String name;
 	private String phoneNumber;
 	private String id;
@@ -12,42 +14,48 @@ public class User {
 
 	private double saveTime; // 잔여 시간
 	private double totalTime; // 누적 시간
-	
-	public User(String name, String phoneNumber, String id, String password, String securitNumber){
-		this.name = name; 
+
+	public User(String name, String phoneNumber, String id, String password, String securitNumber) {
+		this.name = name;
 		this.phoneNumber = phoneNumber;
 		this.id = id;
 		this.password = password;
 		this.securitNumber = securitNumber;
 	}
-	
+
 	public String getName() {
 		return name;
 	}
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
+
 	public String getId() {
 		return id;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public String getSecuritNumber() {
 		return securitNumber;
 	}
+
 	public int getAge() {
 		return age;
 	}
+
 	public boolean isLogin() {
 		return isLogin;
 	}
+
 	public double getSaveTime() {
 		return saveTime;
 	}
+
 	public double getTotalTime() {
 		return totalTime;
 	}
-	
-	
 }
