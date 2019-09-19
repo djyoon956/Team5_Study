354a0f0d18c40e0ea070dec723f77765126e1963
dajeong.yoon <djyoon956@gmail.com>
2019-09-19 19:57:58 +0900

    USer 생성자 변경

--- src/Team5_Final/User.java
+++ src/Team5_Final/User.java
@@ -1,6 +1,9 @@
 package Team5_Final;
 
 import java.io.Serializable;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class User implements Serializable {
 	private String name;
@@ -8,20 +11,20 @@ public class User implements Serializable {
 	private String id;
 	private String password;
 	private String securitNumber;
-	private int money;
 	private int age;
-
 	private boolean isLogin;
-
 	private double saveTime; // 잔여시간
 	private double totalTime; // 누적시간
+	private String joinDay; // 가입일
 
-	public User(String name, String phoneNumber, int age, String password, String securitNumber) {
+	public User(String name, String phoneNumber, String id, String password, String securitNumber) {
 		this.name = name;
 		this.phoneNumber = phoneNumber;
-		this.age = age;
+		this.id = id;
 		this.password = password;
 		this.securitNumber = securitNumber;
+		// 나이해줘야함
+		this.joinDay = setJoinDay();
 	}
 
 	public String getName() {
@@ -52,7 +55,6 @@ public class User implements Serializable {
 		return age;
 	}
 
-	
 	public void setLogin(boolean isLogin) {
 		this.isLogin = isLogin;
 	}
@@ -69,10 +71,21 @@ public class User implements Serializable {
 		return totalTime;
 	}
 
+	public String getJoinDay() {
+		return joinDay;
+	}
+
+	public void setJoinDay(String joinDay) {
+		this.joinDay = joinDay;
+	}
+
 	@Override
 	public String toString() {
 		return "이름 : " + name + " / " + "전화번호 : " + phoneNumber + " / " + "나이: " + id + " / " + "비밀번호 : " + password
 				+ " / " + "주민번호 : " + securitNumber;
 	}
 
+	private String setJoinDay() {
+		return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
+	}
 }
