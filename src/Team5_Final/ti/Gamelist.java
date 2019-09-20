5f450817dd3b1191acbbe38556f460ccc74c85f9
권태일 <aptlehsk@naver.com>
2019-09-20 14:20:10 +0900

    커밋

--- src/Team5_Final/ti/Gamelist.java
+++ src/Team5_Final/ti/Gamelist.java
@@ -3,13 +3,13 @@ package Team5_Final.ti;
 
 public class Gamelist {
 	
-	   	gamelist[]  ga= {new Person(),new Person(), new Person()};"배틀 그라운드", "카운터 스트라이크", "LOL", "카트라이더" , "스타크래프트" , "서든어택" , "오버워치"};
-	    char[] ch = new char[10];
-		for(int j = 0; j < ch.length ; j++)
-		{
-		System.out.println(">" + ch[j] + "<");
-		}
+	String gamename;
 	
+	Gamelist[] glist = new Gamelist[10]
+	
+	glist[0].gamename = "배틀 그라운드";
+	
+	Gamelist[] gamelist = {new game("배틀 그라운드"), new game("카운터 스트라이크"),   
 		
 		
 	}
