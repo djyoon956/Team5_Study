708cd741468168758840db31d77aecf604557b84
권태일 <aptlehsk@naver.com>
2019-09-20 15:31:39 +0900

    커밋

--- src/Team5_Final/ti/printGameList.java
+++ src/Team5_Final/ti/printGameList.java
@@ -1,10 +1,15 @@
 package Team5_Final.ti;
+
+import java.util.ArrayList;
+
 //게임 목록 기능
 
 public class printGameList {
-
+	ArrayList<> arr = new ArrayList<>();
+	
 		void gamelist(String gamename) {
 			this.gamelist(gamename);
+	
 			
 			
 		}
