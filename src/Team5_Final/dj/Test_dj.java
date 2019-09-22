c42f8d66ad47cff5ef7fa98d77cdebdbaf5c0729
dajeong.yoon <djyoon956@gmail.com>
2019-09-22 22:23:53 +0900

    개발용 패키지 삭제

--- src/Team5_Final/dj/Test_dj.java
+++
@@ -1,8 +0,0 @@
-package Team5_Final.dj;
-
-public class Test_dj {
-	public static void main(String[] args) {
-		PCmanagement p = new PCmanagement();
-		p.start();
-	}
-}
--- src/Team5_Final/hy/Test_hy.java
+++
@@ -1,7 +0,0 @@
-package Team5_Final.hy;
-
-public class Test_hy {
-	public static void main(String[] args) {
-// test
-	}
-}
